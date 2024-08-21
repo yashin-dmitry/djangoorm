c111d483283bbc7ea256f91efe7b9d9b531c9589
Dmitry <exigo53@gmail.com>
2024-08-21 20:55:31 +0300

    ex 1 final

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+def index(request):
+    return render(request, 'main/index.html')
+
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
-    return render(request, 'main/index.html', context)
-
+    return render(request, 'main/product_detail.html', context)
--- orm/urls.py
+++ orm/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
