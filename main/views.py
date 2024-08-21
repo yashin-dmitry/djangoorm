367927f168b10f004e3aca81208ae6dc8f8b7f38
Dmitry <exigo53@gmail.com>
2024-08-21 22:47:48 +0300

    ex 2

--- main/views.py
+++ main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def index(request):
-    return render(request, 'main/index.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'main/index.html', context)
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
